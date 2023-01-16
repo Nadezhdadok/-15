98434ffd5ca2b526aeb43c0534f245b7a90a624d
homeworkdome <homeworkdome@outlook.com>
2023-01-16 16:34:23 +0300

    Добавьте файлы проекта.

---
+++ Задача 15/Program.cs
@@ -0,0 +1,11 @@
+﻿using Задача_15;
+
+GeomProgression geom = new GeomProgression();
+geom.setStart(10);
+geom.setStep(2);
+Console.WriteLine(geom.getNext());
+Console.WriteLine(geom.getNext());
+Console.WriteLine(geom.getNext());
+geom.reset();
+Console.WriteLine(geom.getNext());
+Console.WriteLine(geom.getNext());
